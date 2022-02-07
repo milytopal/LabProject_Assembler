5170bb54a1cd41a1d601d8b2d1600674a513f220
milytopal <mily.topal@gmail.com>
2022-02-07 19:50:33 +0200

    added files

---
+++ cmake-build-debug/CMakeFiles/Project_maman14.dir/dataLinkedList.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/Project_maman14.dir/dataLinkedList.c.o: \
+  /Users/milytopal/CLionProjects/Project_maman14/dataLinkedList.c \
+  /Users/milytopal/CLionProjects/Project_maman14/dataLinkedList.h \
+  /Users/milytopal/CLionProjects/Project_maman14/dataStructs.h
---
+++ cmake-build-debug/CMakeFiles/Project_maman14.dir/parseCodeToFile.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/Project_maman14.dir/parseCodeToFile.c.o: \
+  /Users/milytopal/CLionProjects/Project_maman14/parseCodeToFile.c \
+  /Users/milytopal/CLionProjects/Project_maman14/parseCodeToFile.h
