5cdf3e6d4796df7731e626a1699b5d2f21318554
LuckyCqke <ozi.aharonson@gmail.com>
2022-02-09 21:06:37 +0200

    a

--- dataLinkedList.c
+++ dataLinkedList.c
@@ -1,5 +1,23 @@
 //
 // Created by Mily Topal on 06/02/2022.
-//
+// <-- Ubuntu gives an error on this kind of comment, might wanna change it
 #include "dataLinkedList.h"
+
+Node* get(List list, int index){
+    if (index > list.length) {
+        return;
+    }
+
+    Node *node = list.head;
+    int i = 0;
+    for (; i < index; ++i) {
+        node = node->next;
+    }
+    return node;
+}
+
+void add(List list, Node toAdd){
+    get(list, list.length)->next = &toAdd;
+}
+
 
--- dataLinkedList.h
+++ dataLinkedList.h
@@ -6,8 +6,21 @@
 #define DATA_LINKED_LIST_H
 #include "dataStructs.h"
 
+#define NODE_STR_LENGTH 20
+
+typedef struct Node{
+    char data[NODE_STR_LENGTH];
+    struct Node *next;
+} Node;
+
+typedef struct List{
+    Node *head;
+    int length;
+} List;
 
 
+void add(List, Node);
+Node* get(List, int);
 
 
 
