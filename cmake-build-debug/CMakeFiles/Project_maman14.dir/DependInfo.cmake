aa5e04e5c964093c365d2c3236843205de9b4b42
milytopal <mily.topal@gmail.com>
2022-03-03 19:37:52 +0200

    added test case

---
+++ cmake-build-debug/CMakeFiles/Project_maman14.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/milytopal/CLionProjects/Project_maman14/dataLinkedList.c" "/Users/milytopal/CLionProjects/Project_maman14/cmake-build-debug/CMakeFiles/Project_maman14.dir/dataLinkedList.c.o"
+  "/Users/milytopal/CLionProjects/Project_maman14/firstPass.c" "/Users/milytopal/CLionProjects/Project_maman14/cmake-build-debug/CMakeFiles/Project_maman14.dir/firstPass.c.o"
+  "/Users/milytopal/CLionProjects/Project_maman14/main.c" "/Users/milytopal/CLionProjects/Project_maman14/cmake-build-debug/CMakeFiles/Project_maman14.dir/main.c.o"
+  "/Users/milytopal/CLionProjects/Project_maman14/parseCodeToFile.c" "/Users/milytopal/CLionProjects/Project_maman14/cmake-build-debug/CMakeFiles/Project_maman14.dir/parseCodeToFile.c.o"
+  "/Users/milytopal/CLionProjects/Project_maman14/preProcessor.c" "/Users/milytopal/CLionProjects/Project_maman14/cmake-build-debug/CMakeFiles/Project_maman14.dir/preProcessor.c.o"
+  "/Users/milytopal/CLionProjects/Project_maman14/secondPass.c" "/Users/milytopal/CLionProjects/Project_maman14/cmake-build-debug/CMakeFiles/Project_maman14.dir/secondPass.c.o"
+  "/Users/milytopal/CLionProjects/Project_maman14/utils.c" "/Users/milytopal/CLionProjects/Project_maman14/cmake-build-debug/CMakeFiles/Project_maman14.dir/utils.c.o"
+  "/Users/milytopal/CLionProjects/Project_maman14/wordLinkedList.c" "/Users/milytopal/CLionProjects/Project_maman14/cmake-build-debug/CMakeFiles/Project_maman14.dir/wordLinkedList.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Project_maman14.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_maman14.dir/dataLinkedList.c.o"
+  "CMakeFiles/Project_maman14.dir/firstPass.c.o"
+  "CMakeFiles/Project_maman14.dir/main.c.o"
+  "CMakeFiles/Project_maman14.dir/parseCodeToFile.c.o"
+  "CMakeFiles/Project_maman14.dir/preProcessor.c.o"
+  "CMakeFiles/Project_maman14.dir/secondPass.c.o"
+  "CMakeFiles/Project_maman14.dir/utils.c.o"
+  "CMakeFiles/Project_maman14.dir/wordLinkedList.c.o"
+  "Project_maman14"
+  "Project_maman14.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project_maman14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
